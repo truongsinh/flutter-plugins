e922e158ff09a13366b013543758f5886cc2a71d
TruongSinh Tran-Nguyen <i@truongsinh.pro>
2019-01-10 14:55:14 +0700

    format

--- packages/android_intent/test/dummy_test.dart
+++ packages/android_intent/test/dummy_test.dart
@@ -4,11 +4,11 @@ import 'package:android_intent/android_intent.dart';
 
 void main() {
   test('dummy', () async {
-    final AndroidIntent _ =  const AndroidIntent(
+    final AndroidIntent _ = const AndroidIntent(
       action: 'action_view',
       data: 'https://play.google.com/store/apps/details?'
           'id=com.google.android.apps.myapp',
       arguments: <String, dynamic>{},
-  );
+    );
   });
 }
